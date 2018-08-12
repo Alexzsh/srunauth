4ff9962d5008b0c24b62757002b76effc95b8dd9
alexzsh <zjutzsh@gmail.com>
2018-08-12 15:54:24 +0800

    update toast via wifiable

--- LoginScreenApp 15.43.46 15.50.40/ViewController.swift
+++ LoginScreenApp 15.43.46 15.50.40/ViewController.swift
@@ -13,26 +13,20 @@ class ViewController: UIViewController, UITextFieldDelegate, UITextViewDelegate 
     var command = "do_login"
     let login_url = "http://192.0.0.6/cgi-bin/"
     var uid = "1234"
-    
+
     
     @IBOutlet var username_input: UITextField!
     @IBOutlet var password_input: UITextField!
     @IBOutlet var login_button: UIButton!
+    @IBOutlet var logout_button: UIButton!
     
-    var login_session:String = ""
+    var localAddress:String = ""
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
         username_input.text = "linshi120"
         password_input.text = "654321"
-            
-        
-        
-        
-        
     }
     // return 取消textfield的第一相应
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
@@ -51,10 +45,10 @@ class ViewController: UIViewController, UITextFieldDelegate, UITextViewDelegate 
         reachability.whenReachable = { reachability in
             if reachability.connection == .wifi {
                 //self.showToast(message: ("Connected WiFi"))
-                let status = self.do_login(username:self.username_input.text!, password: self.password_input.text!,foo: self.command)
+                let status = self.do_login(username:self.username_input.text!, password: self.password_input.text!,command: self.command,connectStatus: reachability.isReachableViaWiFi)
                 self.getRes(datastring: status, command: self.command)
             } else {
-                self.showToast(message: ("Please connect Cellular"))
+                self.showToast(message: ("Please connect wifi"))
             }
         }
         reachability.whenUnreachable = { _ in
@@ -67,38 +61,108 @@ class ViewController: UIViewController, UITextFieldDelegate, UITextViewDelegate 
             print("Unable to start notifier")
         }
         
+        
+    }
+    func getLocalIPAddressForCurrentWiFi() -> String? {
+        
+        var address: String?
+        
+        
+        
+        // get list of all interfaces on the local machine
+        
+        var ifaddr: UnsafeMutablePointer<ifaddrs>? = nil
+        
+        guard getifaddrs(&ifaddr) == 0 else {
+            
+            return nil
+            
+        }
+        
+        guard let firstAddr = ifaddr else {
+            
+            return nil
+            
+        }
+        
+        for ifptr in sequence(first: firstAddr, next: { $0.pointee.ifa_next }) {
+            
+            
+            
+            let interface = ifptr.pointee
+            
+            
+            
+            // Check for IPV4 or IPV6 interface
+            
+            let addrFamily = interface.ifa_addr.pointee.sa_family
+            
+            if addrFamily == UInt8(AF_INET) || addrFamily == UInt8(AF_INET6) {
+                
+                // Check interface name
+                
+                let name = String(cString: interface.ifa_name)
+                
+                if name == "en0" {
+                    
+                    // Convert interface address to a human readable string
+                    
+                    var addr = interface.ifa_addr.pointee
+                    
+                    var hostName = [CChar](repeating: 0, count: Int(NI_MAXHOST))
+                    
+                    getnameinfo(&addr,
+                                
+                                socklen_t(interface.ifa_addr.pointee.sa_len),
+                                
+                                &hostName, socklen_t(hostName.count), nil, socklen_t(0), NI_NUMERICHOST)
+                    
+                    address = String(cString: hostName)
+                    
+                }
+                
+            }
+            
+        }
+        
+        freeifaddrs(ifaddr)
+        
+        return address
+        
     }
     @IBAction func DoLogin(_ sender: AnyObject) {
         UIApplication.shared.keyWindow?.endEditing(true)//通用的隐藏键盘的方式
+        self.command="do_login"
         checkWifi()
         
     }
-    
+    @IBAction func DoLogout(_ sender: AnyObject) {
+        UIApplication.shared.keyWindow?.endEditing(true)//通用的隐藏键盘的方式
+        self.command="do_logout"
+        checkWifi()
+        
+    }
     func getRes(datastring:String,command:String){
-        if command=="do_login"{
-            if datastring.IsDigit(){
-                self.showToast(message: "Login Success" as! String)
-                uid=datastring
-                
-                LoginDone()
-            }
-            else {
-                self.showToast(message: datastring )
-            }
+        if command=="do_login" && datastring.IsDigit(){
+                self.showToast(message: "Login Success" )
+                self.uid=datastring
         }
-        else {
-            if datastring == "logout_ok" {
-                LoginToDo()
-            }
-            self.showToast(message: datastring )
-        }
+        self.showToast(message: datastring )
+        
     }
     
     
     
-    func do_login(username:String,password:String,foo:String) -> String{
+    func do_login(username:String,password:String,command:String,connectStatus:Bool) -> String{
+        if connectStatus && command=="do_login"  {
+            return("Had logined")
+        }
+        else if !connectStatus && command=="do_logout"{
+            return("Had not logined")
+        }
+
         let passwd_md5 = password.md5()[NSRange(location: 8, length: 16)]
-        let access_url:String = login_url+foo
+        let access_url:String = login_url+command
         
         var url:NSURL!
         url = NSURL(string:access_url)
@@ -131,27 +195,7 @@ class ViewController: UIViewController, UITextFieldDelegate, UITextViewDelegate 
     
     
     
-    func LoginDone()
-    {
-//        username_input.isEnabled = false
-//        password_input.isEnabled = false
-        
-//        login_button.isEnabled = true
-        command="do_logout"
-        
-        login_button.setTitle("Logout", for: .normal)
-    }
-    
-    func LoginToDo()
-    {
-//        username_input.isEnabled = true
-//        password_input.isEnabled = true
-        
-//        login_button.isEnabled = true
-        
-        command="do_login"
-        login_button.setTitle("Login", for: .normal)
-    }
+   
 }
 
 extension UIViewController {
